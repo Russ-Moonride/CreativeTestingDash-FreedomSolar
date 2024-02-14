30c4ad9e195db77c267dc8d8cdea9b288e6f3a87
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-14 11:08:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import requests
 import json
 from google.cloud import storage
 
+
 credentials = service_account.Credentials.from_service_account_info(
           st.secrets["gcp_service_account"]
       )
