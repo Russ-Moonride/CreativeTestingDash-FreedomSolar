7a5d9d7b40ef59f8049a615f48506d75d35f2ce3
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-14 11:00:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -396,7 +396,8 @@ def main_dashboard():
           
             # Concatenate aggregated_data with total_df
             final_df = pd.concat([aggregated_data, total_df])
-          
+
+            st.write(final_df)
             # Initialize an empty list to store significance results
             significance_results = []
             
